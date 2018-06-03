d3e33e04d6d7f2d89db4af42c460fa6df71bb730
vanverr <vaneverr@student.arteveldehs.be>
2018-06-03 18:32:44 +0200

    push 16
    styleguide

---
+++ pages/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {	
+	$("#foto1").hover(function(){
+		$(this).css("background-color", "hsla(166,64%,56%,1)");
+		$("#text-block1").css("background-color", "hsla(166,64%,56%,1)");
+		$("#text-block1").css("color", "rgb(91,122,140)");
+		}, function(){
+		$(this).css("background-color", "rgb(91,122,140)");
+		$("#text-block1").css("background-color", "rgb(91,122,140)");
+		$("#text-block1").css("color", "hsla(166,64%,56%,1)");
+	});
+ });
+  $(document).ready(function() {	
+	$("#foto2").hover(function(){
+		$(this).css("background-color", "hsla(166,64%,56%,1)");
+		$("#text-block2").css("background-color", "hsla(166,64%,56%,1)");
+		$("#text-block2").css("color", "rgb(91,122,140)");
+		}, function(){
+		$(this).css("background-color", "rgb(91,122,140)");
+		$("#text-block2").css("background-color", "rgb(91,122,140)");
+		$("#text-block2").css("color", "hsla(166,64%,56%,1)");
+	});
+ });
+  $(document).ready(function() {	
+	$("#foto3").hover(function(){
+		$(this).css("background-color", "hsla(166,64%,56%,1)");
+		$("#text-block3").css("background-color", "hsla(166,64%,56%,1)");
+		$("#text-block3").css("color", "rgb(91,122,140)");
+		}, function(){
+		$(this).css("background-color", "rgb(91,122,140)");
+		$("#text-block3").css("background-color", "rgb(91,122,140)");
+		$("#text-block3").css("color", "hsla(166,64%,56%,1)");
+	});
+ });
+
